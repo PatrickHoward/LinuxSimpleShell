c6b69099d7d8ba161457376204f9b3fb5bac30a5
Patrick M. Howard <pmh41@zips.uakron.edu>
2019-05-02 10:47:25 -0400

    Update projectx

--- projectX.c
+++ projectX.c
@@ -2,6 +2,7 @@
 // Created by Patrick M. Howard - pmh41@zips.uakron.edu
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -21,36 +22,10 @@ typedef struct Command
 
 } Command;
 
-void command_parseCommand(Command* command, char* userInput, char delim)
+void command_newCommand(Command* command)
 {
-    //I would put them in directly into a command type object, but I'm lazy...
-    //TODO: Remove token array and place information directly into command object.
-    char* tokens[MAXARGS];
-    int numberOfTokens = 0;
-
-    tokens[numberOfTokens] = userInput;
-
-    while(*userInput != '\0')
-    {
-        if(*userInput == delim)
-        {
-            *userInput = '\0';
-            ++userInput;
-
-            tokens[++numberOfTokens] = userInput;
-        }
-        ++userInput;
-    }
-
-    strcpy(command->cmdName, tokens[0]);
-    command->argCount = numberOfTokens - 1;
-    for(int i = 0; i < numberOfTokens - 1; ++i)
-    {
-        strcpy(command->argValues[i], tokens[i]);
-    }
-
-        command->argValues[numberOfTokens] = (void*)0;
-
+    command->cmdName = (char*) malloc(MAXARGLEN);
+    command->binName = (char*) malloc(MAXARGLEN);
 }
 
 void command_clear(Command* command)
@@ -61,6 +36,99 @@ void command_clear(Command* command)
     command->argCount = 0;
 }
 
+void command_parseCommand(Command* command, char* userInput, char delim)
+{
+    char* tokens[MAXARGS];
+    int numberOfTokens = 0;
+
+    tokens[numberOfTokens++] = userInput;
+
+    while(*userInput != '\0')
+    {
+        if(*userInput == delim)
+        {
+            *userInput = '\0';
+            ++userInput;
+
+            tokens[numberOfTokens++] = userInput;
+        }
+        else
+        {
+            ++userInput;
+        }
+    }
+
+    command->cmdName = tokens[0];
+    command->argCount = numberOfTokens - 1;
+    for(int i = 1; i < numberOfTokens; ++i)
+    {
+        command->argValues[i - 1] = tokens[i];
+    }
+
+    command->argValues[numberOfTokens - 1] = (void*)0;
+
+}
+
+void command_findbin(Command* command)
+{
+    if(strcmp(command->cmdName, "C"))
+    {
+        strcpy(command->binName, "cp");
+    }
+    else if(strcmp(command->cmdName, "D"))
+    {
+        strcpy(command->binName, "rm");
+    }
+    else if(strcmp(command->cmdName, "E"))
+    {
+        strcpy(command->binName, "echo");
+    }
+    else if(strcmp(command->cmdName, "H"))
+    {
+        strcpy(command->binName, "help");
+    }
+    else if(strcmp(command->cmdName, "L"))
+    {
+        strcpy(command->binName, "ls");
+    }
+    else if(strcmp(command->cmdName, "M"))
+    {
+        strcpy(command->binName, "nano");
+    }
+    else if(strcmp(command->cmdName, "P"))
+    {
+        strcpy(command->binName, "cat");
+    }
+    else if(strcmp(command->cmdName, "Q"))
+    {
+        strcpy(command->binName, "exit");
+    }
+    else if(strcmp(command->cmdName, "S"))
+    {
+        strcpy(command->binName, "firefox");
+    }
+    else if(strcmp(command->cmdName, "W"))
+    {
+        strcpy(command->binName, "clear");
+    }
+    else if(strcmp(command->cmdName, "X"))
+    {
+        strcpy(command->binName, command->argValues[0]);
+    }
+
+    // if(command->argCount == 0)
+    // {
+    //     command->argValues[0] = '\0';
+    // }
+}
+
+void command_runbin(Command* command)
+{
+    
+    execvp(command->binName, command->argValues);
+
+}
+
 void printPrompt(char* userName, char* hostName)
 {
     printf("linux - %s@%s - > ", userName, hostName);
@@ -68,13 +136,14 @@ void printPrompt(char* userName, char* hostName)
 
 void getInput(char* userInput)
 {
-        fgets(userInput, LINELEN, stdin);
+    fgets(userInput, LINELEN, stdin);
 }
 
 int main()
 {
-    char* usr = getlogin();
+    char sysUser[1024];
     char sysHost[1024];
+    getlogin_r(sysUser, 1024);
     gethostname(sysHost, 1024);
 
     char buffer[LINELEN];
@@ -82,11 +151,16 @@ int main()
 
     while(1)
     {
-        printPrompt(usr, sysHost);
+        printPrompt(sysUser, sysHost);
         getInput(buffer);
 
-        command_parseCommand(&activeCommand, buffer, ' ');
-        printf("%s %s\n", activeCommand.cmdName, activeCommand.argValues[0]);
+        command_clear(&activeCommand);
+        command_newCommand(&activeCommand);
+        command_parseCommand(&activeCommand, buffer, STD_PARSER_DELIMITER);
+        command_findbin(&activeCommand);
+        printf("%s\n", activeCommand.binName);
+        
+        command_runbin(&activeCommand);
     }
 
 }
